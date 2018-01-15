83420799896b982c22519bd3f3c89f217eaa8609
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-15 16:47:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+function displayError() {
+  document.getElementById("errors").innerHTML = "error";
+}
+
 $(document).ready(function (){
 });
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
